0fd78975e2259c1939d149ae7e7689fb7d6625ce
David Lin <David.Lin@bmwna.com>
2016-11-08 19:09:55 -0600

    initial project files

---
+++ Delayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Delayer
+//
+//  Created by Lin David, US-205 on 11/8/16.
+//  Copyright © 2016 Lin David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
